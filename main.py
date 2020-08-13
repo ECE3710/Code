a9dfeef08dda1f1a21bef37be9f8669577fbf261
GarrettMatthews <garrettemathews@gmail.com>
2020-08-13 16:50:47 -0600

    Adding case_ratio, region_average, and state_graph to the main function

--- main.py
+++ main.py
@@ -5,15 +5,23 @@ from forecast import forecastGrowth
 from forecast_graph import graphForecast
 from actual_graph import graphActual
 from stay_at_home import stayAtHome
+import case_ratio
+import state_graph
+import region_average
+
+case_ratio.main()
 
 statesData = pd.read_csv('../Data/states.csv')
 states = statesData['name']
 
 for state in states:
-  forecastGrowth(state)
-  graphForecast(state)
-  graphActual(state)
-  stayAtHome(state)
-  plt.legend()
-  plt.savefig(f'../Data/Graphs/{state}_graph.png')
-  plt.clf()
+    forecastGrowth(state)
+    graphForecast(state)
+    graphActual(state)
+    stayAtHome(state)
+    plt.legend()
+    plt.savefig(f'../Data/Graphs/{state}_graph.png')
+    plt.clf()
+
+region_average.main()
+state_graph.main()
